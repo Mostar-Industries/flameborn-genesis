52f62a3b1664c2b6425f95ae631db59982970c7a
Mo-101 <akiniobong10@gmail.com>
2025-06-11 08:10:46 +0300

    ok

--- FlameBornToken.sol
+++ FlameBornToken.sol
@@ -16,11 +16,11 @@ contract FlameBornToken is
     ERC20Pausable,
     AccessControl
 {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER");
 
-    event MinterGranted(address indexed newMinter);
-    event PauserGranted(address indexed newPauser);
+    event MinterGranted(address indexed account);
+    event PauserGranted(address indexed account);
 
     constructor(address admin)
         ERC20("FlameBorn Token", "FLB")
@@ -45,56 +45,56 @@ contract FlameBornToken is
         _unpause();
     }
 
-    function grantMinter(address newMinter) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        require(newMinter != address(0), "Invalid address");
-        _grantRole(MINTER_ROLE, newMinter);
-        emit MinterGranted(newMinter);
+    function grantMinter(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(account != address(0), "Invalid address");
+        _grantRole(MINTER_ROLE, account);
+        emit MinterGranted(account);
     }
 
-    function grantPauser(address newPauser) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        require(newPauser != address(0), "Invalid address");
-        _grantRole(PAUSER_ROLE, newPauser);
-        emit PauserGranted(newPauser);
+    function grantPauser(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(account != address(0), "Invalid address");
+        _grantRole(PAUSER_ROLE, account);
+        emit PauserGranted(account);
     }
 
-    /// @dev Override required due to multiple inheritance of _update from ERC20, ERC20Pausable, and ERC20Votes
-    function _update(address from, address to, uint256 value) internal virtual override(ERC20, ERC20Pausable, ERC20Votes) {
-        super._update(from, to, value);
-    }
-
-    // Override required by Solidity for multiple inheritance
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
-        internal
-        override(ERC20, ERC20Pausable)
+    // Only need to override the "most derived" contracts for each hook.
+    function _update(address from, address to, uint256 value)
+    internal
+    virtual
+        override(ERC20, ERC20Pausable, ERC20Votes)
     {
-        super._beforeTokenTransfer(from, to, amount);
+        super._update(from, to, value);
     }
 
     function _afterTokenTransfer(address from, address to, uint256 amount)
         internal
-        override(ERC20, ERC20Votes)
+        virtual
+        override(ERC20, ERC20Pausable, ERC20Votes)
     {
         super._afterTokenTransfer(from, to, amount);
     }
 
     function _mint(address to, uint256 amount)
         internal
-        override(ERC20, ERC20Votes)
+        virtual
+        override(ERC20, ERC20Pausable, ERC20Votes)
     {
         super._mint(to, amount);
     }
 
     function _burn(address account, uint256 amount)
         internal
-        override(ERC20, ERC20Votes)
+        virtual
+        override(ERC20, ERC20Pausable, ERC20Votes)
     {
         super._burn(account, amount);
     }
 
+    // OpenZeppelin v5: nonces is in Nonces, not ERC20Permit
     function nonces(address owner)
         public
         view
-        override(ERC20Permit)
+        override(Nonces)
         returns (uint256)
     {
         return super.nonces(owner);
